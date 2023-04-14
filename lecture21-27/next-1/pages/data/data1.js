ebe05b516a97eef19720d8dd44ddcf4d8b6f77e6
rupesh kumar <rudhg35@gmail.com>
2023-04-14 17:54:24 +0530

    nextJs File base routing

---
+++ lecture21-27/next-1/pages/data/data1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const data1 = () => {
+  return (
+    <div>data1</div>
+  )
+}
+
+export default data1
---
+++ lecture21-27/next-1/pages/data/data2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const data2 = () => {
+  return (
+    <div>data2</div>
+  )
+}
+
+export default data2
